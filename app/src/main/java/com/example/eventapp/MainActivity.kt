20c92a8ca81680b2a18b180c028dc32c9b8e3adb
m29dev <michalmajchrzak.it@gmail.com>
2024-04-21 17:20:30 +0200

    signin feature

--- app/src/main/java/com/example/eventapp/MainActivity.kt
+++ app/src/main/java/com/example/eventapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.eventapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -12,13 +13,17 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() , View.OnClickListener {
 
-    lateinit var btnAdd : Button
-    lateinit var btnSub : Button
-    lateinit var btnMul : Button
-    lateinit var btnDiv : Button
-    lateinit var etA : EditText
-    lateinit var etB : EditText
-    lateinit var resultTv : TextView
+//    lateinit var btnAdd : Button
+//    lateinit var btnSub : Button
+//    lateinit var btnMul : Button
+//    lateinit var btnDiv : Button
+//    lateinit var etA : EditText
+//    lateinit var etB : EditText
+//    lateinit var resultTv : TextView
+
+    lateinit var etLogin : EditText
+    lateinit var etPassword : EditText
+    lateinit var btnSignIn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,18 +31,24 @@ class MainActivity : AppCompatActivity() , View.OnClickListener {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        btnAdd = findViewById(R.id.btn_add)
-        btnSub = findViewById(R.id.btn_subtraction)
-        btnMul = findViewById(R.id.btn_multiplication)
-        btnDiv = findViewById(R.id.btn_division)
-        etA =  findViewById(R.id.et_a)
-        etB = findViewById(R.id.et_b)
-        resultTv = findViewById(R.id.result_tv)
+//        btnAdd = findViewById(R.id.btn_add)
+//        btnSub = findViewById(R.id.btn_subtraction)
+//        btnMul = findViewById(R.id.btn_multiplication)
+//        btnDiv = findViewById(R.id.btn_division)
+//        etA =  findViewById(R.id.et_a)
+//        etB = findViewById(R.id.et_b)
+//        resultTv = findViewById(R.id.result_tv)
 
-        btnAdd.setOnClickListener(this)
-        btnSub.setOnClickListener(this)
-        btnMul.setOnClickListener(this)
-        btnDiv.setOnClickListener(this)
+        etLogin = findViewById(R.id.et_login)
+        etPassword = findViewById(R.id.et_password)
+        btnSignIn = findViewById(R.id.btn_signin)
+
+//        btnAdd.setOnClickListener(this)
+//        btnSub.setOnClickListener(this)
+//        btnMul.setOnClickListener(this)
+//        btnDiv.setOnClickListener(this)
+
+        btnSignIn.setOnClickListener(this)
 
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -47,25 +58,36 @@ class MainActivity : AppCompatActivity() , View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        var a = etA.text.toString().toDouble()
-        var b = etB.text.toString().toDouble()
-        var result = 0.0
+        var login = etLogin.text.toString()
+        var password = etPassword.text.toString()
 
         when(v?.id){
-            R.id.btn_add -> {
-                result = a+b
-            }
-            R.id.btn_subtraction -> {
-                result = a-b
-            }
-            R.id.btn_multiplication -> {
-                result = a*b
-            }
-            R.id.btn_division -> {
-                result = a/b
+            R.id.btn_signin -> {
+                Log.i("Credentials", "Login: $login, Password: $password")
             }
         }
-
-        resultTv.text = "Result is $result"
     }
+
+//    override fun onClick(v: View?) {
+//        var a = etA.text.toString().toDouble()
+//        var b = etB.text.toString().toDouble()
+//        var result = 0.0
+//
+//        when(v?.id){
+//            R.id.btn_add -> {
+//                result = a+b
+//            }
+//            R.id.btn_subtraction -> {
+//                result = a-b
+//            }
+//            R.id.btn_multiplication -> {
+//                result = a*b
+//            }
+//            R.id.btn_division -> {
+//                result = a/b
+//            }
+//        }
+//
+//        resultTv.text = "Result is $result"
+//    }
 }
